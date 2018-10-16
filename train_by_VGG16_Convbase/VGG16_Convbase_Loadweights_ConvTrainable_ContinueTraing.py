daefcc13ff0c02506b32196f39b8f01019ec42e7
MUJZY <zhengning009@sina.com>
2018-10-16 13:06:12 +0800

    Add files via upload

---
+++ train_by_VGG16_Convbase/VGG16_Convbase_Loadweights_ConvTrainable_ContinueTraing.py
@@ -0,0 +1,151 @@
+# encoding=utf-8
+# Date: 2018-10-14
+# Author: MJUZY
+# Important Referrence:
+# https://blog.csdn.net/zpalyq110/article/details/80432827
+# https://github.com/Freemanzxp/Image-category-understanding-and-application/blob/master/main/MedicalLargeFine_tuning.py
+
+
+import os
+from keras.preprocessing.image import ImageDataGenerator
+from Prepare import base_dir
+
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(224, 224),
+    batch_size=20,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(224, 224),
+    batch_size=20,
+    class_mode='categorical')
+
+
+from keras.applications import VGG16
+from keras.models import Model
+from keras.layers import Dense
+from keras import optimizers
+
+
+from keras.models import load_model
+model = load_model('VGG16FineTune_8_Clothes_Classes.h5')
+
+for i in range(len(model.layers)):
+    if i <= 10:
+        model.layers[i].trainable = False
+    else:
+        model.layers[i].trainable = True
+"""
+    >>>for layer in model.layers[:10]: layer.trainable = False
+"""
+
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+    loss='categorical_crossentropy',
+    metrics=['acc'])
+
+model.summary()
+
+history = model.fit_generator(
+                    train_generator,
+                    steps_per_epoch=100,  # Stands for the total times of the training loop
+                    epochs=50,
+                    validation_data=validation_generator,
+                    validation_steps=50)
+
+model.save('VGG16FineTune_8_Clothes_Classes_ConvTrainable_moretime.h5')
+
+import matplotlib.pyplot as plt
+
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and Validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+""" Appendix:
+
+Layer (type)                 Output Shape              Param #   
+=================================================================
+input_1 (InputLayer)         (None, 224, 224, 3)       0         
+_________________________________________________________________
+block1_conv1 (Conv2D)        (None, 224, 224, 64)      1792      
+_________________________________________________________________
+block1_conv2 (Conv2D)        (None, 224, 224, 64)      36928     
+_________________________________________________________________
+block1_pool (MaxPooling2D)   (None, 112, 112, 64)      0         
+_________________________________________________________________
+block2_conv1 (Conv2D)        (None, 112, 112, 128)     73856     
+_________________________________________________________________
+block2_conv2 (Conv2D)        (None, 112, 112, 128)     147584    
+_________________________________________________________________
+block2_pool (MaxPooling2D)   (None, 56, 56, 128)       0         
+_________________________________________________________________
+block3_conv1 (Conv2D)        (None, 56, 56, 256)       295168    
+_________________________________________________________________
+block3_conv2 (Conv2D)        (None, 56, 56, 256)       590080    
+_________________________________________________________________
+block3_conv3 (Conv2D)        (None, 56, 56, 256)       590080    
+_________________________________________________________________
+block3_pool (MaxPooling2D)   (None, 28, 28, 256)       0         
+_________________________________________________________________
+block4_conv1 (Conv2D)        (None, 28, 28, 512)       1180160   
+_________________________________________________________________
+block4_conv2 (Conv2D)        (None, 28, 28, 512)       2359808   
+_________________________________________________________________
+block4_conv3 (Conv2D)        (None, 28, 28, 512)       2359808   
+_________________________________________________________________
+block4_pool (MaxPooling2D)   (None, 14, 14, 512)       0         
+_________________________________________________________________
+block5_conv1 (Conv2D)        (None, 14, 14, 512)       2359808   
+_________________________________________________________________
+block5_conv2 (Conv2D)        (None, 14, 14, 512)       2359808   
+_________________________________________________________________
+block5_conv3 (Conv2D)        (None, 14, 14, 512)       2359808   
+_________________________________________________________________
+block5_pool (MaxPooling2D)   (None, 7, 7, 512)         0         
+_________________________________________________________________
+flatten (Flatten)            (None, 25088)             0         
+_________________________________________________________________
+dense_1 (Dense)              (None, 256)               6422784   
+_________________________________________________________________
+dense_2 (Dense)              (None, 8)                 2056      
+=================================================================
+Total params: 21,139,528
+Trainable params: 13,504,264
+Non-trainable params: 7,635,264
+"""
